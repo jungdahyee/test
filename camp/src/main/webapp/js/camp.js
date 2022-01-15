306105dc31978af733d0c84aecd3bd13b584f4dd
DESKTOP-08J8C5F\ekgp4 <dh2806@naver.com>
2022-01-15 17:07:23 +0900

    다혜~

---
+++ camp/src/main/webapp/js/camp.js
@@ -0,0 +1,43 @@
+
+function joinFormCheck(){
+    let id, pass, cpass, name, hp1,hp2,hp3,email1,email2,email3;
+    id=document.getElementById("id");
+    pass=document.getElementById("pass");
+    cpass=document.getElementById("cpass");
+    name=document.getElementById("name");
+    hp1=document.getElementById("hp1");
+    hp2=document.getElementById("hp2");
+    hp3=document.getElementById("hp3");
+    email1=document.getElementById("email1");
+    email2=document.getElementById("email2");
+    email3=document.getElementById("email3");
+    
+    if(id.value==""){
+        alert("아이디");
+    }else if(pass.value==""){
+        alert("비번");
+    }else if(cpass.value==""){
+        alert("비번확인");
+    }else if(name.value==""){
+        alert("name");
+    }else if(hp1.value==""&&hp2.value==""&&hp3.value==""){
+        alert("hp");
+    }else if(email1.value==""&&email2.value==""&&email3.value==""){
+        alert("email");
+    }
+    
+}
+
+function passCheck(){
+    let id;
+    id=document.getElementById("id");
+    if(id.value==""){
+       document.getElementById("id_space").innerHTML="아이디는 필수로 입력해야합니다.";
+       id.focus();
+    }else{
+        document.getElementById("id_space").innerHTML="";
+    }
+    
+    
+}
+
---
+++ camp/src/main/webapp/main.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content = "width=device-width", initial-scale="1">
+<title>Insert title here</title>
+<link href="css/campyo.css" rel="stylesheet">
+</head>
+<body>
+<iframe class="h" src="header.jsp" scrolling="no" width="100%" height="90"></iframe>
+<div class="slideShow">
+   <ul class="slidelist">
+        <li><img alt="tent" src="images/tent_crop.jpg" ></li>
+        <li><img alt="glam" src="images/glam_crop.jpg" ></li>
+        <li><img alt="car" src="images/car_crop.jpg" ></li>
+        <li><img alt="trailer" src="images/trailer_crop.jpg"></li>
+    </ul>
+</div>
+<iframe class="f" src="footer.jsp" scrolling="no" width="100%" height="50"></iframe>
+<script src="js/camp.js"></script>
+</body>
+</html>
