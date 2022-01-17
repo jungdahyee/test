4a258cc9f16f350c3bef6f3904e4e9227d353314
kimsia0717 <https://Kimsia0717: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@github.com/Kimsia0717/teamC_campYo>
2022-01-17 17:13:26 +0900

    각 테이블 DTO 추가

---
+++ camp/src/main/java/com/example/demo/memberDTO.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+public class memberDTO {
+    String user_id, user_pass, user_name, user_tel, user_email, sdate, user_exityn;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_pass() {
+        return user_pass;
+    }
+
+    public void setUser_pass(String user_pass) {
+        this.user_pass = user_pass;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_tel() {
+        return user_tel;
+    }
+
+    public void setUser_tel(String user_tel) {
+        this.user_tel = user_tel;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public String getSdate() {
+        return sdate;
+    }
+
+    public void setSdate(String sdate) {
+        this.sdate = sdate;
+    }
+
+    public String getUser_exityn() {
+        return user_exityn;
+    }
+
+    public void setUser_exityn(String user_exityn) {
+        this.user_exityn = user_exityn;
+    }
+
+}
