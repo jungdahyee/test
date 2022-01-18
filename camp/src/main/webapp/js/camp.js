f1089f58ae191f94bcf72cc51408bfd7f0501ba7
DESKTOP-1S1Q32C\82103 <tksska5414@naver.com>
2022-01-18 17:36:06 +0900

    별거없음

--- camp/src/main/webapp/js/camp.js
+++ camp/src/main/webapp/js/camp.js
@@ -22,7 +22,7 @@ function loginFormCheck(){
 
 
 function joinFormCheck(){
-    let id, pass, cpass, name, hp1,hp2,hp3,email1,email2;
+    let id, pass, cpass, name, hp1,hp2,hp3,email1,email2,tel, email;
     id=document.getElementById("id");
     pass=document.getElementById("pass");
     cpass=document.getElementById("cpass");
@@ -32,33 +32,44 @@ function joinFormCheck(){
     hp3=document.getElementById("hp3");
     email1=document.getElementById("email1");
     email2=document.getElementById("email2");
-
+    
     
     if(id.value==""){
         document.getElementById("join_message").innerHTML="아이디를 입력해주세요";
         id.focus();
+        return false;
     }else if(pass.value==""){
         document.getElementById("join_message").innerHTML="비밀번호를 입력해주세요";
         pass.focus();
+        return false;
     }else if(cpass.value==""){
         document.getElementById("join_message").innerHTML="비밀번호를 입력해주세요";
         cpass.focus();
+        return false;
     }else if(name.value==""){
         document.getElementById("join_message").innerHTML="이름을 입력해주세요";
         name.focus();
+        return false;
     }else if(hp2.value==""){
         document.getElementById("join_message").innerHTML="중간번호를 입력해주세요";
         hp2.focus();
+        return false;
     }else if(hp3.value==""){
         document.getElementById("join_message").innerHTML="마지막번호를 입력해주세요";
         hp3.focus();
+        return false;
     }else if(email1.value==""){
         document.getElementById("join_message").innerHTML="이메일을 입력해주세요";
         email1.focus();
+        return false;
     }else if(email2.value==""){
         document.getElementById("join_message").innerHTML="이메일주소를 입력해주세요";
         email2.focus();
+        return false;
+    }else{
+        joinForm.submit();
     }
+    
     
 }
 
